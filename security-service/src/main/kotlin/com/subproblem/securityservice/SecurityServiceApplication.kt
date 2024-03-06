502399cae295b85fa0d8b378a4264521e31c5d31
SubProblem <gukag6@gmail.com>
2024-03-06 13:56:16 +0400

    add security service

---
+++ security-service/src/main/kotlin/com/subproblem/securityservice/SecurityServiceApplication.kt
@@ -0,0 +1,13 @@
+package com.subproblem.securityservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@EnableDiscoveryClient
+@SpringBootApplication
+class SecurityServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<SecurityServiceApplication>(*args)
+}
