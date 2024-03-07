f6bd725a31fb255b49a41279e410b6a600be57fb
SubProblem <gukag6@gmail.com>
2024-03-08 00:46:56 +0400

    add gateway service

---
+++ gateway-service/src/main/kotlin/com/subproblem/gatewayservice/GatewayServiceApplication.kt
@@ -0,0 +1,13 @@
+package com.subproblem.gatewayservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient
+
+@EnableDiscoveryClient
+@SpringBootApplication
+class GatewayServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<GatewayServiceApplication>(*args)
+}
