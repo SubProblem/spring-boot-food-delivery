51b84e6ac060b64f8b9672e25c13e454e263d8c2
SubProblem <gukag6@gmail.com>
2024-03-08 15:14:53 +0400

    add restaurant service

---
+++ restaurant-service/src/test/kotlin/com/subproblem/restaurantservice/RestaurantServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.subproblem.restaurantservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RestaurantServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
