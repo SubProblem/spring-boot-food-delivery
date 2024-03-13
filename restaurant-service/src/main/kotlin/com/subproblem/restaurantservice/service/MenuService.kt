6109a0b150ad9c3f674ffb988b1feed0c9e99bfd
SubProblem <gukag6@gmail.com>
2024-03-14 02:17:36 +0400

    add Menu service

---
+++ restaurant-service/src/main/kotlin/com/subproblem/restaurantservice/service/MenuService.kt
@@ -0,0 +1,69 @@
+package com.subproblem.restaurantservice.service
+
+import com.subproblem.restaurantservice.dto.request.MenuRequestDTO
+import com.subproblem.restaurantservice.dto.response.MenuResponseDTO
+import com.subproblem.restaurantservice.repository.MenuItemRepository
+import com.subproblem.restaurantservice.repository.MenuRepository
+import com.subproblem.restaurantservice.repository.RestaurantRepository
+import com.subproblem.restaurantservice.util.RequestMapper
+import com.subproblem.restaurantservice.util.ResponseMapper
+import jakarta.persistence.EntityNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+
+@Service
+class MenuService(
+    private val restaurantRepository: RestaurantRepository,
+    private val menuRepository: MenuRepository,
+    private val requestMapper: RequestMapper,
+    private val responseMapper: ResponseMapper
+) {
+
+    fun getAllMenus(): ResponseEntity<List<MenuResponseDTO>> {
+        val menus = menuRepository.findAll()
+        val menuResponseToList = responseMapper.menuResponseToList(menus)
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(menuResponseToList)
+    }
+
+    fun getAllMenusWithItems(): ResponseEntity<List<MenuResponseDTO>> {
+        val menus = menuRepository.findAllWithMenuItems()
+        val menuResponseToList = responseMapper.menuResponseToList(menus)
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(menuResponseToList)
+    }
+
+    fun getMenusByRestaurant(restaurantId: Int): ResponseEntity<List<MenuResponseDTO>> {
+
+        val restaurant = restaurantRepository.findById(restaurantId)
+            .orElseThrow { NoSuchElementException("restaurant not found") }
+
+        val menus = menuRepository.findAllByRestaurant(restaurant)
+
+        val response = responseMapper.menuResponseToList(menus)
+
+        return ResponseEntity
+            .status(HttpStatus.OK)
+            .body(response)
+    }
+
+
+    fun addMenu(request: MenuRequestDTO): ResponseEntity<HttpStatus> {
+
+        val restaurant = restaurantRepository.findById(request.restaurantId)
+            .orElseThrow { EntityNotFoundException("Restaurant not found") }
+
+        val menu = requestMapper.requestToMenu(request, restaurant)
+
+        menuRepository.save(menu)
+
+        return ResponseEntity
+            .status(HttpStatus.CREATED)
+            .build()
+    }
+}
