7eec32d0ab1775d2b0aa47514fabe1bb23061fd1
SubProblem <gukag6@gmail.com>
2024-03-19 23:28:09 +0400

    add JPQL query to find menu items by multiple IDs

--- restaurant-service/src/main/kotlin/com/subproblem/restaurantservice/repository/MenuItemRepository.kt
+++ restaurant-service/src/main/kotlin/com/subproblem/restaurantservice/repository/MenuItemRepository.kt
@@ -2,6 +2,9 @@ package com.subproblem.restaurantservice.repository
 
 import com.subproblem.restaurantservice.entity.MenuItem
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface MenuItemRepository : JpaRepository<MenuItem, Int> {
+    @Query("SELECT M FROM MENUITEM M WHERE m.id IN :ids")
+    fun findByIds(ids: List<Int>): List<MenuItem>
 }
