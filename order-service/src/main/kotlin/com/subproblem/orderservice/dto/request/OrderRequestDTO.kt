588564f4f2d9c00ba3a96b2e94eb539b5eea454c
SubProblem <gukag6@gmail.com>
2024-03-19 22:05:25 +0400

    remove default value for createdAt in OrderRequestDTO

--- order-service/src/main/kotlin/com/subproblem/orderservice/dto/request/OrderRequestDTO.kt
+++ order-service/src/main/kotlin/com/subproblem/orderservice/dto/request/OrderRequestDTO.kt
@@ -4,10 +4,8 @@ import java.math.BigDecimal
 import java.time.LocalDateTime
 
 data class OrderRequestDTO(
-    val userId: Int,
     val restaurantId: Int,
     val status: String,
     val totalAmount: BigDecimal,
-    val createdAt: LocalDateTime = LocalDateTime.now(),
     val orderItems: List<OrderItemRequestDTO>
 )
