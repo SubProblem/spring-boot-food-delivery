00bcdda7460271a5a56ead136c10ec5e77ba0341
SubProblem <gukag6@gmail.com>
2024-03-20 19:48:54 +0400

    update findByIds query to use Projection

--- restaurant-service/src/main/kotlin/com/subproblem/restaurantservice/repository/RestaurantRepository.kt
+++ restaurant-service/src/main/kotlin/com/subproblem/restaurantservice/repository/RestaurantRepository.kt
@@ -1,7 +1,7 @@
 package com.subproblem.restaurantservice.repository
 
+import com.subproblem.restaurantservice.dto.RestaurantProjection
 import com.subproblem.restaurantservice.entity.Restaurant
-import kotlinx.coroutines.flow.Flow
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
@@ -17,9 +17,11 @@ interface RestaurantRepository : JpaRepository<Restaurant,Int> {
 
     @Query("SELECT DISTINCT r FROM Restaurant r LEFT JOIN FETCH r.menus")
     fun findAllWithMenus(): List<Restaurant>
-    
-    @Query("SELECT r FROM RESTAURANT r WHERE r.id IN :ids ")
-    fun findByIds(ids: List<Int>): List<Restaurant>
 
-
+    @Query("""
+        SELECT NEW com.subproblem.restaurantservice.dto.RestaurantProjection(r.id, r.name, r.description, r.address, r.phone, r.email)
+        FROM Restaurant r
+        WHERE r.id IN :ids
+    """)
+    fun findByIds(ids: List<Int>): List<RestaurantProjection>
 }
