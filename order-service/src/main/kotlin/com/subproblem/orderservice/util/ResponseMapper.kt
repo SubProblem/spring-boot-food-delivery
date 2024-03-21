a008425c99399c2f2a2578f9c55d773d1e2f4d80
SubProblem <gukag6@gmail.com>
2024-03-21 18:21:00 +0400

    add mapping methods to ResponseMapper class

--- order-service/src/main/kotlin/com/subproblem/orderservice/util/ResponseMapper.kt
+++ order-service/src/main/kotlin/com/subproblem/orderservice/util/ResponseMapper.kt
@@ -1,7 +1,11 @@
 package com.subproblem.orderservice.util
 
+import com.subproblem.orderservice.dto.MenuItem
+import com.subproblem.orderservice.dto.Restaurant
 import com.subproblem.orderservice.dto.response.OrderItemResponseDTO
+import com.subproblem.orderservice.dto.response.OrderItemResponseWithMenuItemDTO
 import com.subproblem.orderservice.dto.response.OrderResponseDTO
+import com.subproblem.orderservice.dto.response.OrderResponseWithRestaurantDTO
 import com.subproblem.orderservice.entity.Order
 import com.subproblem.orderservice.entity.OrderItem
 import org.springframework.stereotype.Service
@@ -42,4 +46,53 @@ class ResponseMapper {
             orderToResponseDTO(it)
         }.toList()
     }
+
+
+    fun toOrderItemResponseWithMenuItem(orderItem: OrderItem, menuItem: MenuItem): OrderItemResponseWithMenuItemDTO {
+        return OrderItemResponseWithMenuItemDTO(
+            orderItem.id,
+            orderItem.order.id,
+            menuItem,
+            orderItem.quantity,
+            orderItem.unitPrice
+        )
+    }
+
+    fun toOrderItemResponseWithMenuItemList(orderItems: List<OrderItem>, menuItems: List<MenuItem>): List<OrderItemResponseWithMenuItemDTO> {
+        return orderItems.mapNotNull { orderItem ->
+            menuItems.find { it.id == orderItem.menuItemId }?.let { menuItem ->
+                toOrderItemResponseWithMenuItem(orderItem, menuItem)
+            }
+        }
+    }
+
+    fun toOrderResponseWithRestaurants(order: Order, restaurant: Restaurant, orderItems: List<OrderItem>, menuItems: List<MenuItem>): OrderResponseWithRestaurantDTO {
+        return OrderResponseWithRestaurantDTO(
+            order.id,
+            order.userId,
+            restaurant,
+            order.status.name,
+            order.totalAmount,
+            order.createdAt,
+            toOrderItemResponseWithMenuItemList(orderItems, menuItems)
+        )
+    }
+
+    fun toOrderResponseWithRestaurantsList(orders: List<Order>, restaurants: List<Restaurant>, allOrderItems: List<OrderItem>, allMenuItems: List<MenuItem>): List<OrderResponseWithRestaurantDTO> {
+        return orders.map { order ->
+            val restaurant = restaurants.find { it.id == order.restaurantId }
+            val items = allOrderItems.filter { it.order.id == order.id }
+            val menuItemList = allMenuItems.filter { it.menuId == order.restaurantId }
+            OrderResponseWithRestaurantDTO(
+                id = order.id,
+                userId = order.userId,
+                restaurant = restaurant ?: Restaurant(0, "", "", "", "", ""), // Provide default Restaurant if not found
+                status = order.status.name,
+                totalAmount = order.totalAmount,
+                createdAt = order.createdAt,
+                orderItems = toOrderItemResponseWithMenuItemList(items, menuItemList)
+            )
+        }
+    }
+
 }
