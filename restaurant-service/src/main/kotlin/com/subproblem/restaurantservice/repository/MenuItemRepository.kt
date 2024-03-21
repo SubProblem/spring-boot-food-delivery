4ec9655b7edc7c23424a845c24d37b1623c29260
SubProblem <gukag6@gmail.com>
2024-03-21 18:05:08 +0400

    update findByIds method in MenuItemRepository to use projection

--- restaurant-service/src/main/kotlin/com/subproblem/restaurantservice/repository/MenuItemRepository.kt
+++ restaurant-service/src/main/kotlin/com/subproblem/restaurantservice/repository/MenuItemRepository.kt
@@ -1,10 +1,17 @@
 package com.subproblem.restaurantservice.repository
 
+import com.subproblem.restaurantservice.dto.MenuItemProjection
 import com.subproblem.restaurantservice.entity.MenuItem
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface MenuItemRepository : JpaRepository<MenuItem, Int> {
-    @Query("SELECT m FROM MenuItem m WHERE m.id IN :ids")
-    fun findByIds(ids: List<Int>): List<MenuItem>
+    @Query("""
+        SELECT NEW com.subproblem.restaurantservice.dto.MenuItemProjection(
+        m.id, m.menu.id, m.name, m.description, m.price
+        )
+        FROM MenuItem m
+        WHERE m.id IN :ids
+        """)
+    fun findByIds(ids: List<Int>): List<MenuItemProjection>
 }
