6d65ea280f520a6e88e0e9d5f8c0e6b9f8b3e67c
SubProblem <gukag6@gmail.com>
2024-03-21 17:50:31 +0400

    add endpoint to retrieve orders with restaurants and menu items

---
+++ order-service/src/main/kotlin/com/subproblem/orderservice/controller/OrderController.kt
@@ -0,0 +1,44 @@
+package com.subproblem.orderservice.controller
+
+import com.subproblem.orderservice.dto.request.OrderRequestDTO
+import com.subproblem.orderservice.dto.response.OrderResponseDTO
+import com.subproblem.orderservice.entity.Order
+import com.subproblem.orderservice.service.OrderService
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.math.BigDecimal
+
+@RestController
+@RequestMapping("/api/v1/orders")
+class OrderController(private val orderService: OrderService) {
+
+
+    @GetMapping
+    fun getAllOrders(request: HttpServletRequest): ResponseEntity<List<OrderResponseDTO>> {
+        val userId = request.getHeader("auth-header-id").toInt()
+        return orderService.getAllOrdersForUser(userId)
+    }
+
+    @GetMapping("/totalAmount")
+    fun getTotalAmountSpent(request: HttpServletRequest): ResponseEntity<BigDecimal> {
+        val userId = request.getHeader("auth-header-id").toInt()
+        return orderService.getTotalAmountSpentByUser(userId)
+    }
+
+    @PostMapping
+    fun makeOrder(@RequestBody order: OrderRequestDTO, request: HttpServletRequest): ResponseEntity<OrderResponseDTO> {
+        val userId = request.getHeader("auth-header-id").toInt()
+        return orderService.makeOrder(userId, order)
+    }
+
+    @GetMapping("/full")
+    suspend fun getOrdersWithRestaurantsAndMenuItems(request: HttpServletRequest): ResponseEntity<Any> {
+        val userId = request.getHeader("auth-header-id").toInt()
+        return orderService.getOrdersWithRestaurantsAndMenuItems(userId)
+    }
+}
