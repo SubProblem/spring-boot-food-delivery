19bd62753a0646eff6025d1658f397ad693d1421
SubProblem <gukag6@gmail.com>
2024-03-22 23:21:29 +0400

    remove orderId field from OrderItemRequestDTO

--- order-service/src/main/kotlin/com/subproblem/orderservice/dto/request/OrderItemRequestDTO.kt
+++ order-service/src/main/kotlin/com/subproblem/orderservice/dto/request/OrderItemRequestDTO.kt
@@ -3,7 +3,6 @@ package com.subproblem.orderservice.dto.request
 import java.math.BigDecimal
 
 data class OrderItemRequestDTO(
-    val orderId: Int,
     val menuItemId: Int,
     val quantity: Int,
     val unitPrice: BigDecimal
