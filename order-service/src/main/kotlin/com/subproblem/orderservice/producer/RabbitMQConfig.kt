37bc5ac05e915e0b96d00223af3b08136d50de49
SubProblem <gukag6@gmail.com>
2024-04-23 19:29:21 +0400

    add RabbitMQ configuration

--- order-service/src/main/kotlin/com/subproblem/orderservice/producer/RabbitMQConfig.kt
+++ order-service/src/main/kotlin/com/subproblem/orderservice/producer/RabbitMQConfig.kt
@@ -1,10 +1,50 @@
 package com.subproblem.orderservice.producer
 
-import org.springframework.amqp.core.Queue
+
+import org.springframework.amqp.core.*
+import org.springframework.amqp.rabbit.connection.ConnectionFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter
+import org.springframework.amqp.support.converter.MessageConverter
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
+
 @Configuration
-class TopicConfig {
+class RabbitMQConfig(
+    @Value("\${spring.rabbitmq.queue.name}")
+    private val queueName: String,
+    @Value("\${spring.rabbitmq.exchange.name}")
+    private val topicExchange: String,
+    @Value("\${spring.rabbitmq.routing.key}")
+    private val routingName: String,
+) {
+
+    @Bean
     fun queue(): Queue =
-        Queue("orderQueue")
+        Queue(queueName, true)
+
+    @Bean
+    fun topicExchange(): TopicExchange =
+        TopicExchange(topicExchange)
+
+    @Bean
+    fun binding(queue: Queue, topicExchange: TopicExchange): Binding =
+        BindingBuilder
+            .bind(queue())
+            .to(topicExchange())
+            .with(routingName)
+
+    @Bean
+    fun template(connectionFactory: ConnectionFactory): AmqpTemplate {
+        val rabbitTemplate = RabbitTemplate(connectionFactory)
+        rabbitTemplate.messageConverter = jsonMessageConverter()
+        return rabbitTemplate
+    }
+
+    @Bean
+    fun jsonMessageConverter(): MessageConverter =
+        Jackson2JsonMessageConverter()
+
 }
